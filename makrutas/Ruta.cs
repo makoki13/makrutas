5d0fd2632bee19e9151d83389252ddeaac5b62ed
makoki <pablo.prieto@dulcesol.es>
2016-05-03 16:42:47 +0200

    Añadir constructores y propiedades

--- makrutas/Ruta.cs
+++ makrutas/Ruta.cs
@@ -8,5 +8,24 @@ namespace makrutas
 {
     class Ruta
     {
+        String id;        
+        String nombre;
+        String usuario;
+        DateTime fechaCreacion;
+        DateTime? fechaPrimeraRealizacion;
+        
+        Ruta()
+        {
+            this.id = "1";
+            this.nombre = "sin nombre";
+            this.usuario = "root";
+            this.fechaCreacion = DateTime.Now;
+            this.fechaPrimeraRealizacion = null; 
+        }
+
+        Ruta(String id)
+        {
+            this.id = id;
+        }
     }
 }
